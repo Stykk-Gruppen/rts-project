d7b25dd22cb3d6aca6c67d0f72b1eca833620076
Kent Odde <kentodde89@gmail.com>
2020-09-17 12:18:05 +0200

    Changed gpr file

--- Arudino_Nano_BLE_Sense/boards/NRF52_DK/obj/zfp_Debug/crt0.d
+++ Arudino_Nano_BLE_Sense/boards/NRF52_DK/obj/zfp_Debug/crt0.d
@@ -1,1 +1,1 @@
-crt0.o: /home/kent/Source/School/RealTimeProject/Ada_Drivers_Library/boards/NRF52_DK/src/zfp/crt0.S 20200916092946
+crt0.o: /home/kent/Source/School/RealTimeProject/rts-project/Arudino_Nano_BLE_Sense/boards/NRF52_DK/src/zfp/crt0.S 20200916092946
