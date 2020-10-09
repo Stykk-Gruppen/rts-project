fec05979e5943c07a1925d319754836ab7ea8619
Tarald <taraldv@gmail.com>
2020-10-09 17:29:20 +0200

    ultrasonic initial test works with hardcoded if

--- testing/ultrasonic_sensor/src/main.adb
+++ testing/ultrasonic_sensor/src/main.adb
@@ -1,20 +1,59 @@
+with Arduino_Nano_33_Ble_Sense.LEDs;
 with Arduino_Nano_33_Ble_Sense.IOs;
-with Ada.Text_IO;
-with HCSR04;
+with Ada.Real_Time; use Ada.Real_Time;
 
 procedure Main is
    TestingSensorIn : Arduino_Nano_33_Ble_Sense.IOs.Pin_Id := 3;
-   TestingSensorOut : Arduino_Nano_33_Ble_Sense.IOs.Pin_Id := 4;
+   TestingSensorOut : Arduino_Nano_33_Ble_Sense.IOs.Pin_Id := 4; --gul
+   Test : Arduino_Nano_33_Ble_Sense.IOs.Pin_Id := 5;
    DistanceValue : Float;
+   TimeNow : Ada.Real_Time.Time := Ada.Real_Time.Clock;
+   Tall : Integer := 0;
+   Pulse : Boolean;
+   Stop : Boolean := False;
 begin
 
    loop
-      --Trenger kanskje ikke init.
-      HCSR04.Init(TestingSensorOut, TestingSensorIn);
+      TimeNow := Ada.Real_Time.Clock;
+      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(TestingSensorOut, False);
+      delay until TimeNow + Ada.Real_Time.Microseconds(2);
+      
+      TimeNow := Ada.Real_Time.Clock;
+      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(TestingSensorOut, True);
+      delay until TimeNow + Ada.Real_Time.Microseconds(10);
+      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(TestingSensorOut, False);
 
-      DistanceValue := HCSR04.Distance(TestingSensorOut, TestingSensorIn);
+      Until_Loop :
+      loop
+         Pulse := Arduino_Nano_33_Ble_Sense.IOs.DigitalRead(TestingSensorIn);
+         TimeNow := Ada.Real_Time.Clock;
+         delay until TimeNow + Ada.Real_Time.Microseconds(1);
+         if Pulse then
+            Stop := True;
+            Tall := Tall + 1;
+         else
+            exit Until_Loop when Stop;
+         end if;
 
-      Ada.Text_IO.Put_Line(DistanceValue'Image);
+         
+
+      end loop Until_Loop;
+      
+      
+      
+      
+      TimeNow := Ada.Real_Time.Clock;
+      delay until TimeNow + Ada.Real_Time.Milliseconds(1000);
+      if Tall >= 2 then
+       Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(Test, True);
+      --else
+       --  Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(Test, True);
+      end if;
+      -- Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(Test, True);
+      TimeNow := Ada.Real_Time.Clock;
+      --delay until TimeNow + Ada.Real_Time.Milliseconds(1000);
+
+
    end loop;
 
 end Main;
