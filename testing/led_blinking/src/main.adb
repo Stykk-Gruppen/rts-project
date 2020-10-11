2241361144edf13fcf5619b358651ced64a00bed
Tarald <taraldv@gmail.com>
2020-10-11 22:38:45 +0200

    updated blink example to blink ALL pins

--- testing/led_blinking/src/main.adb
+++ testing/led_blinking/src/main.adb
@@ -1,25 +1,21 @@
-with Arduino_Nano_33_Ble_Sense.LEDs;
 with Arduino_Nano_33_Ble_Sense.IOs;
-with Arduino_Nano_33_Ble_Sense.Time;
+with Arduino_Nano_33_Ble_Sense.IOs2;
 with Ada.Real_Time; use Ada.Real_Time;
 
 procedure Main is
    TimeNow : Ada.Real_Time.Time := Ada.Real_Time.Clock;
 begin
-   Arduino_Nano_33_Ble_Sense.LEDs.Initialize_LED;
    loop
-      for I in 1 .. 31 loop
-         -- Turns on every pin and the builtin led
-         Arduino_Nano_33_Ble_Sense.LEDs.Turn_On(Arduino_Nano_33_Ble_Sense.LEDs.LED);
+      for I in 0 .. 31 loop
+         Arduino_Nano_33_Ble_Sense.IOs2.DigitalWrite(Arduino_Nano_33_Ble_Sense.IOs2.Pin_Id(I), True);
          Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(Arduino_Nano_33_Ble_Sense.IOs.Pin_Id(I), True);
       end loop;
 
       TimeNow := Ada.Real_Time.Clock;
       delay until TimeNow + Ada.Real_Time.Milliseconds(500);
 
-      for I in 1 .. 31 loop
-         -- Turns off every pin and the builtin led
-         Arduino_Nano_33_Ble_Sense.LEDs.Turn_Off(Arduino_Nano_33_Ble_Sense.LEDs.LED);
+      for I in 0 .. 31 loop
+         Arduino_Nano_33_Ble_Sense.IOs2.DigitalWrite(Arduino_Nano_33_Ble_Sense.IOs2.Pin_Id(I), False);
          Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(Arduino_Nano_33_Ble_Sense.IOs.Pin_Id(I), False);
       end loop;
 
