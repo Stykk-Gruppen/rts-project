453cfad62d6904fd9e8c3db41171c54968798229
Feqzz <stian.onarheim@protonmail.com>
2020-10-15 18:14:32 +0200

    Cleaned up the Ultrasonic sensor testing code

--- testing/ultrasonic_sensor/src/hcsr04.ads
+++ testing/ultrasonic_sensor/src/hcsr04.ads
@@ -1,12 +1,6 @@
 with Arduino_Nano_33_Ble_Sense.IOs;
-----
---with Arduino_Nano_33_Ble_Sense.Time;
---with HAL;
 
 package HCSR04 is
-   package Arduino renames Arduino_Nano_33_Ble_Sense;
-   
-   procedure Init(outPin, inPin : Arduino.IOs.Pin_Id);
-   function Distance(outPin, inPin : Arduino.IOs.Pin_Id) return Float;
 
+   function Distance(outPin, inPin : Arduino_Nano_33_Ble_Sense.IOs.Pin_Id) return Float;
 end HCSR04;
