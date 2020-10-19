1efc84e0a2c5baeea8b813899f4ccc2b319a8bc1
Tarald <taraldv@gmail.com>
2020-10-20 00:15:22 +0200

    juletre funker med oppdatert Ada_Drivers

--- testing/led_blinking/src/main.adb
+++ testing/led_blinking/src/main.adb
@@ -1,21 +1,18 @@
 with Arduino_Nano_33_Ble_Sense.IOs;
-with Arduino_Nano_33_Ble_Sense.IOs2;
 with Ada.Real_Time; use Ada.Real_Time;
 
 procedure Main is
    TimeNow : Ada.Real_Time.Time := Ada.Real_Time.Clock;
 begin
    loop
-      for I in 0 .. 31 loop
-         Arduino_Nano_33_Ble_Sense.IOs2.DigitalWrite(Arduino_Nano_33_Ble_Sense.IOs2.Pin_Id(I), True);
+      for I in 0 .. 47 loop
          Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(Arduino_Nano_33_Ble_Sense.IOs.Pin_Id(I), True);
       end loop;
 
       TimeNow := Ada.Real_Time.Clock;
       delay until TimeNow + Ada.Real_Time.Milliseconds(500);
 
-      for I in 0 .. 31 loop
-         Arduino_Nano_33_Ble_Sense.IOs2.DigitalWrite(Arduino_Nano_33_Ble_Sense.IOs2.Pin_Id(I), False);
+      for I in 0 .. 47 loop
          Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(Arduino_Nano_33_Ble_Sense.IOs.Pin_Id(I), False);
       end loop;
 
