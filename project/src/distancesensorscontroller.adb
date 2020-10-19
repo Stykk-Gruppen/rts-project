8fd5c6068539d5dbd95d8c874ab3f95829eb0279
Stian Onarheim <stian.onarheim@protonmail.com>
2020-10-19 12:51:34 +0200

    Added distance controller

--- project/src/distancesensorscontroller.adb
+++ project/src/distancesensorscontroller.adb
@@ -1,5 +1,13 @@
+with HCSR04;
+
 package body DistanceSensorsController is
 
+   procedure Measure is
+   begin
+      front.value := HCSR04.Distance(front.outPin, front.inPin);
+      back.value := HCSR04.Distance(back.outPin, back.inPin);
+      dispenser.value := HCSR04.Distance(dispenser.outPin, dispenser.inPin);
+   end Measure;
    
 
 end DistanceSensorsController;
--- project/src/distancesensorscontroller.ads
+++ project/src/distancesensorscontroller.ads
@@ -1,27 +1,30 @@
 with Arduino_Nano_33_Ble_Sense.IOs;
-with Arduino_Nano_33_Ble_Sense.Time;
-with HAL;
 
 package DistanceSensorsController is
    package Arduino renames Arduino_Nano_33_Ble_Sense;
 
-   type Distance_Sensor is
-      record
-         outPin : Arduino.IOs.Pin_Id;
-         inPin : Arduino.IOs.Pin_Id;
-      end record;
+   protected type Distance_Sensor is record
+      outPin : Arduino.IOs.Pin_Id;
+      inPin : Arduino.IOs.Pin_Id;
+      value : Float;
+   end record;
    
    -- Begynner først med bare 3stk, da vi ikke helt vet om den kan kjøre på et bord.
    front : Distance_Sensor :=
      (outPin => 0,
-      inPin => 0);
+      inPin => 0,
+      value => 0.0);
                                
    back : Distance_Sensor :=
      (outPin => 0,
-      inPin => 0);
+      inPin => 0,
+      value => 0.0);
       
    dispenser : Distance_Sensor :=
      (outPin => 0,
-      inPin => 0);
+      inPin => 0,
+      value => 0.0);
+   
+   procedure Measure;
    
 end DistanceSensorsController;
