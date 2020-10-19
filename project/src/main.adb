fd8d98cf4b8b19cf659a99da83e65220397f51e7
Kent Odde <kentodde89@gmail.com>
2020-10-19 12:50:42 +0200

    Got sensor and servo tasks working with priority

--- project/src/main.adb
+++ project/src/main.adb
@@ -7,21 +7,16 @@ with Arduino_Nano_33_Ble_Sense.IOs;
 with Ada.Real_Time; use Ada.Real_Time;
 with tasktest;
 
-procedure Main is
-   package Arduino renames Arduino_Nano_33_Ble_Sense;
+procedure Main with Priority => 1 is
    TimeNow : Ada.Real_Time.Time;
 
 begin
 
+
+   loop
    --VehicleController.Init(frontWheelsPin, rearWheelsPin);
-   Arduino.IOs.DigitalWrite (6, True);
-   TimeNow := Ada.Real_Time.Clock;
-   delay until TimeNow + Ada.Real_Time.Microseconds(2000);
 
-   Arduino.IOs.DigitalWrite (6, True);
+      null;
 
-   TimeNow := Ada.Real_Time.Clock;
-   delay until TimeNow + Ada.Real_Time.Microseconds(2000);
-
-
+   end loop;
    end Main;
--- project/src/tasktest.ads
+++ project/src/tasktest.ads
@@ -4,9 +4,9 @@ with HCSR04;
 with Ada.Real_Time; use Ada.Real_Time;
 
 package tasktest is
-   highTime : Integer := 1000 with Atomic;
+   highTime : Integer := 2000 with Atomic;
    Task EngineServo with Priority => 10;
 
-   Task Sensor with Priority => 1;
+   Task Sensor with Priority => 9;
 
 end tasktest;
