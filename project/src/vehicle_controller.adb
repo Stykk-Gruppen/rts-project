03c58ae17da4208a2389410e3f706e227db5471d
Tarald <taraldv@gmail.com>
2020-10-25 20:19:15 +0100

    Implement Brake into FSM

--- project/src/vehicle_controller.adb
+++ project/src/vehicle_controller.adb
@@ -11,18 +11,40 @@ package body Vehicle_Controller is
 
    task body Compute is
       Time_Now : Ada.Real_Time.Time;
+      Front_Blocked : Boolean;
+      Back_Blocked : Boolean;
    begin
       Time_Now := Ada.Real_Time.Clock;
       delay until Time_Now + Ada.Real_Time.Milliseconds(1000);
       loop
-         --Next state logic
-         if Distance_Sensor_Controller.Front.Value < Stop_Distance_Front and Distance_Sensor_Controller.Back.Value < Stop_Distance_Back then
+         Front_Blocked := Distance_Sensor_Controller.Front.Value < Stop_Distance_Front;
+         Back_Blocked := Distance_Sensor_Controller.Back.Value < Stop_Distance_Back;
+
+         --Check braking before sensors, can only enter Stop after braking.
+         if Current_Direction = Braking then
             Next_Direction := Stop;
-            null;
-         elsif Distance_Sensor_Controller.Front.Value < Stop_Distance_Front then
-            Next_Direction := Backward;
-         elsif  Distance_Sensor_Controller.Back.Value < Stop_Distance_Back then
-            Next_Direction := Forward;
+            --If both sensors are blocked, stop, or brake if moving forward.
+         elsif Front_Blocked and Back_Blocked then
+            if Current_Direction = Forward then
+               Next_Direction := Braking;
+            else
+               Next_Direction := Stop;
+            end if;
+            --If only front is blocked, reverse, or brake if moving forward.
+         elsif Front_Blocked then
+             if Current_Direction = Forward then
+               Next_Direction := Braking;
+            else
+               Next_Direction := Backward;
+            end if;
+            --If only back is blocked, go forward, or stop if reversing.
+         elsif  Back_Blocked then
+             if Current_Direction = Backward then
+               Next_Direction := Stop;
+            else
+               Next_Direction := Forward;
+            end if;
+            --Default direction if stopped and both sensors are ok is forward.
          else
             if Current_Direction = Stop then
                Next_Direction := Forward;
@@ -50,22 +72,17 @@ package body Vehicle_Controller is
             Servo_Controller.Engine_Servo.Current_Direction := Servo_Controller.Forward;
             Status_Light_Colour := Green;
          when Backward =>
-            if Current_Direction /= Backward then
-               Status_Light_Colour := Yellow;
-               Servo_Controller.Engine_Servo.Current_Direction := Servo_Controller.Backward;
-               Time_Now := Ada.Real_Time.Clock;
-               delay until Time_Now + Ada.Real_Time.Milliseconds(200);
-               Servo_Controller.Engine_Servo.Current_Direction := Servo_Controller.Stop;
-               Time_Now := Ada.Real_Time.Clock;
-               delay until Time_Now + Ada.Real_Time.Milliseconds(600);
-            end if;
             Servo_Controller.Engine_Servo.Current_Direction := Servo_Controller.Backward;
             Status_Light_Colour := Blue;
          when Stop =>
             Servo_Controller.Engine_Servo.Current_Direction := Servo_Controller.Stop;
+            Time_Now := Ada.Real_Time.Clock;
+            delay until Time_Now + Ada.Real_Time.Milliseconds(600);
             Status_Light_Colour := Red;
          when Braking =>
-            -- It will never end up here.
+            Servo_Controller.Engine_Servo.Current_Direction := Servo_Controller.Backward;
+            Time_Now := Ada.Real_Time.Clock;
+            delay until Time_Now + Ada.Real_Time.Milliseconds(200);
             Status_Light_Colour := Yellow;
          end case;
          Current_Direction := Next_Direction;
@@ -82,6 +99,7 @@ package body Vehicle_Controller is
       Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (24, True);
       Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (16, True);
       Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (6, True);
+      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (13, False);
       loop
          Time_Now := Ada.Real_Time.Clock;
          case Status_Light_Colour is
@@ -98,11 +116,9 @@ package body Vehicle_Controller is
                delay until Time_Now + Ada.Real_Time.Milliseconds(100);
                Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (6, True);
             when Yellow =>
-               Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (24, False);
-               Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (16, False);
+               Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (13, True);
                delay until Time_Now + Ada.Real_Time.Milliseconds(100);
-               Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (24, True);
-               Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (16, True);
+               Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (13, False);
          end case;
          Time_Now := Ada.Real_Time.Clock;
          delay until Time_Now + Ada.Real_Time.Milliseconds(100);
