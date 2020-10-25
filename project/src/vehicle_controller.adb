79222774e4a23323f79874e3a41c8e9e185e996d
Tarald <taraldv@gmail.com>
2020-10-25 20:45:42 +0100

    Forslag til schedule timer for Compute task

--- project/src/vehicle_controller.adb
+++ project/src/vehicle_controller.adb
@@ -1,5 +1,6 @@
 with Distance_Sensor_Controller;
 with Ada.Real_Time; use Ada.Real_Time;
+with Ada.Execution_Time;
 with Arduino_Nano_33_Ble_Sense.IOs;
 with Servo_Controller;
 
@@ -10,10 +11,19 @@ package body Vehicle_Controller is
    -------------
 
    task body Compute is
+      --Only for calculating schedule--
+      StartTimer : Ada.Execution_Time.CPU_Time;
+      EndTimer : Ada.Execution_Time.CPU_Time;
+      ExeTime : Ada.Execution_Time.CPU_Time;
+
       Time_Now : Ada.Real_Time.Time;
       Front_Blocked : Boolean;
       Back_Blocked : Boolean;
    begin
+      --Only for calculating schedule--
+      --Should start at 0 when task is created, and increase only when used by CPU
+      StartTimer := Ada.Execution_Time.Clock;
+
       Time_Now := Ada.Real_Time.Clock;
       delay until Time_Now + Ada.Real_Time.Milliseconds(1000);
       loop
@@ -57,6 +67,8 @@ package body Vehicle_Controller is
          Time_Now := Ada.Real_Time.Clock;
          delay until Time_Now + Ada.Real_Time.Milliseconds(200);
 
+         --Only for calculating schedule--
+         EndTimer := Ada.Execution_Time.Clock;
       end loop;
    end Compute;
 
