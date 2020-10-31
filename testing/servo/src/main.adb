84cc729284fa8798349cfc93193918cdf743a8bf
Tarald <taraldv@gmail.com>
2020-10-31 16:12:51 +0100

    Restored servo example

--- testing/servo/src/main.adb
+++ testing/servo/src/main.adb
@@ -2,90 +2,23 @@ with Arduino_Nano_33_Ble_Sense.IOs;
 with Ada.Real_Time; use Ada.Real_Time;
 
 procedure Main is
-   TimeNow : Ada.Real_Time.Time := Ada.Real_Time.Clock;
-   InnerLoopDuration : Ada.Real_Time.Time := Ada.Real_Time.Clock;
-   Pin_Id : Arduino_Nano_33_Ble_Sense.IOs.Pin_Id := 5;
-   High_Time : Integer := 2000;
+   Time_Now : Ada.Real_Time.Time := Ada.Real_Time.Clock;
+   Rotate_Time : Ada.Real_Time.Time := Ada.Real_Time.Clock;
+   Pin_Id_PWM : Arduino_Nano_33_Ble_Sense.IOs.Pin_Id := 30;
+   High_Time : Integer := 2_000;
 begin
-   High_Time := 1500;
-   Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (24, false);
-   for j in 0 .. 100 loop
-      TimeNow := Ada.Real_Time.Clock;
-      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, True);
-      delay until TimeNow + Ada.Real_Time.Microseconds(High_Time);
-      TimeNow := Ada.Real_Time.Clock;
-      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, false);
-      delay until TimeNow + Ada.Real_Time.Microseconds(20000 - High_Time);
-   end loop;
-   Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (24, true);
-
    loop
+      if Ada.Real_Time.Clock > Rotate_Time + Ada.Real_Time.Milliseconds(1000) then
+         --Changes between 1000 and 2000
+         High_Time := ((High_Time + 1) mod 2000) + 1000;
+         Rotate_Time := Ada.Real_Time.Clock;
+      end if;
+      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(Pin_Id_PWM,True);
+      Time_Now := Ada.Real_Time.Clock;
+      delay until Time_Now + Ada.Real_Time.Microseconds(High_Time);
 
-      --FREMOVER
-      High_Time := 1600;
-      for j in 0 .. 200 loop
-         TimeNow := Ada.Real_Time.Clock;
-         Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, True);
-         delay until TimeNow + Ada.Real_Time.Microseconds(High_Time);
-         TimeNow := Ada.Real_Time.Clock;
-         Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, false);
-         delay until TimeNow + Ada.Real_Time.Microseconds(20000 - High_Time);
-      end loop;
-
-      --FIRST BRAKE CLICK!
-      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (41, true);
-      High_Time := 1400;
-      for j in 0 .. 10 loop
-         TimeNow := Ada.Real_Time.Clock;
-         Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, True);
-         delay until TimeNow + Ada.Real_Time.Microseconds(High_Time);
-         TimeNow := Ada.Real_Time.Clock;
-         Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, false);
-         delay until TimeNow + Ada.Real_Time.Microseconds(20000 - High_Time);
-      end loop;
-      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (41, false);
-
-
-      --IDLE
-      High_Time := 1500;
-      for j in 0 .. 30 loop
-         TimeNow := Ada.Real_Time.Clock;
-         Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, True);
-         delay until TimeNow + Ada.Real_Time.Microseconds(High_Time);
-         TimeNow := Ada.Real_Time.Clock;
-         Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, false);
-         delay until TimeNow + Ada.Real_Time.Microseconds(20000 - High_Time);
-      end loop;
-
-
-      --SECOND CLICK REVERSE
-      High_Time := 1000;
-      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (13, false);
-      for j in 0 .. 200 loop
-         TimeNow := Ada.Real_Time.Clock;
-         Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, True);
-         delay until TimeNow + Ada.Real_Time.Microseconds(High_Time);
-         TimeNow := Ada.Real_Time.Clock;
-         Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, false);
-         delay until TimeNow + Ada.Real_Time.Microseconds(20000 - High_Time);
-      end loop;
-      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (13, true);
-      TimeNow := Ada.Real_Time.Clock;
-      --delay until TimeNow + Ada.Real_Time.Microseconds(2000);
-
-
-
-      --IDLE
-      High_Time := 1500;
-      for j in 0 .. 10 loop
-         TimeNow := Ada.Real_Time.Clock;
-         Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, True);
-         delay until TimeNow + Ada.Real_Time.Microseconds(High_Time);
-         TimeNow := Ada.Real_Time.Clock;
-         Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite (Pin_Id, false);
-         delay until TimeNow + Ada.Real_Time.Microseconds(20000 - High_Time);
-      end loop;
-
+      Arduino_Nano_33_Ble_Sense.IOs.DigitalWrite(Pin_Id_PWM,False);
+      Time_Now := Ada.Real_Time.Clock;
+      delay until Time_Now + Ada.Real_Time.Microseconds(20_000 - High_Time);
    end loop;
-   null;
 end Main;
